65d6a0da2856679ada24c4a318ede643b6d398d9
yasmin <yasminDev2@gmail.com>
2019-03-04 17:13:31 +0530

    HdfcBank,Customer files were created

---
+++ src/com/nt/bank/Customer.java
@@ -0,0 +1,12 @@
+package com.nt.bank;
+
+public class Customer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HdfcBank hdfc=new HdfcBank();
+			
+			hdfc.deposite(10000);
+	}
+
+}
---
+++ src/com/nt/bank/HdfcBank.java
@@ -0,0 +1,13 @@
+package com.nt.bank;
+
+public class HdfcBank {
+	private int accNo;
+	private double bal;
+	private String name;
+	
+	
+	public void deposite(double amt){
+		bal=bal+amt;
+		System.out.println(+amt+ "was deposited to account");
+	}
+}
