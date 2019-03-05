201b38e6dc7d413f5733b266ada56826d7250a3e
yasmin <yasminDev2@gmail.com>
2019-03-05 12:47:19 +0530

    commit after solving conflict

--- src/com/nt/bank/Customer.java
+++ src/com/nt/bank/Customer.java
@@ -4,12 +4,17 @@ package com.nt.bank;
 
 public class Customer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		HdfcBank hdfc=new HdfcBank();
+	
+public static void main(String[] args) {
+	
+	// TODO Auto-generated method stub
+		
+	HdfcBank hdfc=new HdfcBank();
 			
-			hdfc.deposite(10000);
-			hdfc.deposite(7900);
+		
+	  hdfc.deposite(10000);
+			
+	  hdfc.deposite(7900);
 	}
 }
 
